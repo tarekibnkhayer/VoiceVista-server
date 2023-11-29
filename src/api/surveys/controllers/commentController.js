710d199f4d7f52a299449d0af11d488f02dffefa
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-29 08:59:46 +0600

    solved the comment bug(always store comment data in first survey

--- src/api/surveys/controllers/commentController.js
+++ src/api/surveys/controllers/commentController.js
@@ -4,10 +4,8 @@ const commentController = async(req, res) => {
     const newComment = req.body;
     const id = req.query.id;
     try {
-        const query = {_id: id};
-        const specificDocument = await Survey.findOne(query);
-        console.log(specificDocument);
-        
+        const result  = await Survey.updateOne({_id: id}, {$push:{comments: newComment}}); 
+        res.send(result);
     } catch (error) {
         
     }
